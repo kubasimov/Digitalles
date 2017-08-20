51288dcf3f74d6faf22d5d70e62e1e11c80fdba4
KubAsimov <kubasimov@gmail.com>
2017-08-20 18:55:37 +0200

    Changes while writing document

--- Tests/Tests_Core.cs
+++ Tests/Tests_Core.cs
@@ -1,11 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using Core;
 using System.IO;
 using System.Linq;
 using Core.Decode;
+using Core.Interface;
 using Core.Model;
+using Moq;
 using Newtonsoft.Json;
+using Tesseract;
+using WPF.Helpers;
+using WPF.Model;
 using Xunit;
 
 namespace Tests
@@ -15,6 +22,8 @@ namespace Tests
         readonly string _image;
         readonly Start _start = new Start();
 
+        private Dictionary<string, string> _dictionary;
+       
         public TestsCore()
         {
             const string fileName = @"F:/3.jpg";
@@ -23,8 +32,27 @@ namespace Tests
 
             _image=filename;
 
+            LoadDictionaryPassword();
+            
         }
 
+
+        private void LoadDictionaryPassword()
+        {
+            if (File.Exists(@"D:\dane\skroty.json"))
+            {
+                _dictionary =
+                    JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(@"D:\dane\skroty.json"));
+                
+            }
+            else
+            {
+                _dictionary = new Dictionary<string, string>();
+                
+            }
+        }
+
+        
         [Fact]
         public void ReadFileFromStringName()
         {
@@ -54,93 +82,54 @@ namespace Tests
 
 
 
+        
+
+     
         [Fact]
-        public void TestRecognizeText()
+        public void ReadImageToPix()
         {
-            //var text1 =
-            //    "koncentracyjny p.tom III. □ górn. Stół koncentracyjny «urządzenie do wzbogacania drobnych ziarn urobku; nachylona płyta nieruchoma, " +
-            //    "drgająca lub obracająca się, po której wraz z wodą spływa urobek (dzieląc się na ziarna użyteczne i płonne)»";
+            Mock<IReadPicture> mock = new Mock<IReadPicture>();
+            mock.Setup(m => m.ReadImageFromFile("Test")).Returns(Pix.Create(200, 200, 32));
 
-            //var text2 = "koncentrak m III pot. «obóz koncentracyjny»";
+            Ocr ocr = new Ocr(mock.Object);
+            var test = ocr.ReadFile("Test");
 
-            //var text3 =
-            //    "koncentrator m IV, Ms. ~orze elektr.techn. «cewka indukcyjna umożliwiająca skupienie zmiennego strumienia" +
-            //    " magnetycznego wielkiej częstotliwości w określonym miejscu»";
-
-            var text4 =
-                "koncentratywny rzad. «polegający na koncentracji, na skupieniu się»: Polecają(...) metodę autogenicznego" +
-                " treningu, zwanego też koncentratywmym samoodprężeniem albo pospolicie relaksem. Tryb.Ludu 239,1966.";
-
-            //var text5 =
-            //    "konceptualny 1.książk. «dotyczący koncepcji, pojęciowy»: Powieści nie lepi się z samych realiów," +
-            //    " powieść trzeba zbudować, a więc wprowadzić do niej czynnik projektujący, konceptualny.Polit. 32," +
-            //    " 1965. 2.filoz. «odnoszący się do konceptualizmu, związany z konceptualizmem»";
-
-            //var text6= "konceptysta m odm.jak ż IV, CMs. ~yście, lm M. ~yści, DB. - óW lit. «przedstawiciel " +
-            //           "konceptyzmu, poeta piszący kwiecistym stylem; kultysta» // SW w zn. .«konceptualista»";
-
-            //var text11 = text1.Split(' ');
-            //var text21 = text2.Split(' ');
-            //var text31 = text3.Split(' ');
-            var text41 = text4.Split(' ');
-            //var text51 = text5.Split(' ');
-            //var text61 = text6.Split(' ');
-
-            Dictionary<string, string> dic =
-                JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(@"D:\dane\dictionary.json"));
-
-            var result = new Dictionary<string, string>();
-
-            for (var i = 0; i < text41.Length; i++)
-            {
-                if (dic.ContainsKey(text41[i]))
-                {
-                    result.Add(text41[i], dic[text41[i]]);
-                    
-                }
-                else if (text41[i] == "«"||text41[i].First()== '«')
-                {
-                    var temp = "";
-
-                    temp += text41[i];
-                    do
-                    {
-                        i++;
-
-                        temp += " " + text41[i];
-                        
-
-                    } while (!text41[i].Contains('»'));
-
-                    if(temp.Contains(':'))
-                        temp=temp.TrimEnd(':');
-
-                    result.Add(temp,"");
-                    
-                }
-            }
-
+            Assert.True(test);
+            
         }
 
-        [Fact]
-        public void SaveJson()
-        {
-            Dictionary<string, string> katalog = new Dictionary<string, string>
+
+        private readonly string _text = "animalistyka ż III blm szt. " +
+                              "«przedstawianie zwierząt lub motywów zwierzęcych w sztukach plastycznych, " +
+                                        "w fotografice»: Do mistrzostwa doprowadził sztukę fotograficzną," +
+                                        " szczególniej w tak trudnym dziale jak animalistyka (zdjęcia" +
+                                        " zwierząt). Probl. 1954, s. 570. <łc. animal = zwierzę>";
+
+        private readonly ObservableCollection<DictionaryPasswordElement> _result =
+            new ObservableCollection<DictionaryPasswordElement>
             {
-                { "rzad.", "rzadko używany" },
-                { "książk.", "książkowy" },
-                {"pot.", "potoczny"},
-                { "Ms.", "miejscownik" }
+                new DictionaryPasswordElement {Word = "animalistyka", Description = "hasło"},
+                new DictionaryPasswordElement{Word = "ż", Description = "żeński (rodzaj)"},
+                new DictionaryPasswordElement{Word = "III", Description = "III koniugacja/deklinacja"},
+                new DictionaryPasswordElement{Word = "blm", Description = "bez liczby mnogiej"},
+                new DictionaryPasswordElement{Word = "szt.", Description = "bez liczby mnogiej"},
+                new DictionaryPasswordElement{Word = "«przedstawianie zwierząt lub motywów zwierzęcych w " +
+                                                     "sztukach plastycznych, w fotografice»",
+                    Description = "definicja"},
+                new DictionaryPasswordElement{Word = "Do mistrzostwa doprowadził sztukę fotograficzną," +
+                                                     " szczególniej w tak trudnym dziale jak animalistyka" +
+                                                     " (zdjęcia zwierząt). Probl. 1954, s. 570.", Description = "cytat"},
+                new DictionaryPasswordElement{Word = "<łc. animal = zwierzę>",
+                    Description = "wyjaśnienie etymologiczne wyrazu"},
             };
+        
+        [Fact]
+        public void AnalizeRecognizePasswordText()
+        {
+            var test = RecognizePasswordText.Recognize(_text, _dictionary);
 
-
-            //string json = JsonConvert.SerializeObject(katalog, Formatting.Indented);
-
-            //File.WriteAllText(@"D:\dane\dictionary.json", JsonConvert.SerializeObject(katalog, Formatting.Indented));
-
-            Dictionary<string, string> dic =
-                JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(@"D:\dane\dictionary.json"));
-            Assert.Equal(katalog,dic);
+            Assert.Equal(_result,test);
         }
+
     }
 }
